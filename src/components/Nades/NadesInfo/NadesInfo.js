e2cea84919238bd3a63fb1b7d46bb6a3ad02035a
Mesters55 <93780907+Mesters55@users.noreply.github.com>
2022-06-20 12:39:44 +0300

    Update NadesInfo.js

--- src/components/Nades/NadesInfo/NadesInfo.js
+++ src/components/Nades/NadesInfo/NadesInfo.js
@@ -7,7 +7,7 @@ const NadesInfo = () => {
         <div className='summaryBox'>
             <div className='summaryMessage'>
                 <h1 className='nadesInfoH1'>Level 10 Smokes</h1>
-                <p>Lietotāju kopiena paradzēta "CSGO" spēles entuziastiem. Šeit Jūs varat iegūt daudz un dažādu padomu un pilnveidot sevi, kā spēlētāju!</p>
+                <p>asd</p>
             </div>
             <div className='mapLogo'>
                <img className='mapLogoIMG' src = {mirageLogo} alt="mapLogoIMGnotFound" />

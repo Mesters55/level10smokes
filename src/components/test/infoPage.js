b3810c989e5e6e5cc903f194673fc9f3fee6dfb5
Mesters55 <93780907+Mesters55@users.noreply.github.com>
2022-06-26 18:34:14 +0300

    Update infoPage.js

--- src/components/test/infoPage.js
+++ src/components/test/infoPage.js
@@ -26,25 +26,26 @@ const Info = () =>{
                   <div className='col'>
                       <div className="form">
                           <h3 className="h3Name">Sazinies!</h3>
-                          <form className="form">
+                          {showSent ? <div>Vēstule nosūtīta !</div> : <div></div>}
+                          <form className="form" onSubmit={sendEmailHandler}>
                                 <div className = "row">
                                     <div className="colForm">
                                         <label className='formLabel'>Vārds *</label>
-                                        <input type = "text" name = "name" class="inputForm" placeholder="Jūsu vārds" />
+                                        <input type = "text" name = "name" className="inputForm" placeholder="Jūsu vārds" />
                                     </div>
                                     <div className="colForm">
                                         <label className='formLabel'>Ēpasts *</label>
-                                        <input type = "email" name = "email" class="inputForm" placeholder="Jūsu ēpasts" />
+                                        <input type = "email" name = "email" className="inputForm" placeholder="Jūsu ēpasts" />
                                     </div>
                                 </div>
                                 <div className = "row">
                                     <div className="colForm">
                                         <label className='formLabel'>Ziņa *</label>
-                                        <textarea name='message' tpye = 'text' cols="4" rows="4" class="inputForm2" placeholder="Jūsu ziņa..." />
+                                        <textarea name='message' tpye = 'text' cols="4" rows="4" className="inputForm2" placeholder="Jūsu ziņa..." />
                                     </div>
                                 </div>
                                 <div className="row">
-                                    <button class="button-18" role="button">Sūtīt!</button>
+                                    <button type="submit" value ="send" className="button-18" >Sūtīt!</button>
                                 </div>
                           </form>
                       </div>
